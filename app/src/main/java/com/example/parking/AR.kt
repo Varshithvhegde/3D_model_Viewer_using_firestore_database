0a1744b24017ac89bc3a9358e8cee05d18f64d41
varshithvhegde <varshithvh@gmail.com>
2022-06-26 20:04:46 +0530

    App name change

--- app/src/main/java/com/example/parking/AR.kt
+++ app/src/main/java/com/example/parking/AR.kt
@@ -9,7 +9,6 @@ class AR : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ar)
-        
         var message: String? = intent.getStringExtra("message_key")
         (supportFragmentManager.findFragmentById(R.id.arFragment) as ArFragment)
             .setOnTapPlaneGlbModel(message)
